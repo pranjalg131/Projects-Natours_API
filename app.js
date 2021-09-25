cb064bade96f65d972baaac00c96d7f9fc9e8e42
Pranjal Gupta <pranjalg131@gmail.com>
2021-09-25 12:03:49 +0530

    Added create tour and get tour by id

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 const port = 3000;
+
+const PATH = `${__dirname}/dev-data/data/tours-simple.json`;
 
 // app.get('/', (req, res) => {
 //   res
@@ -15,15 +19,42 @@ const port = 3000;
 //   res.send('You can post to this endpoint...');
 // });
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
+const tours = JSON.parse(fs.readFileSync(PATH));
 
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
     data: { tours },
+  });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  } else {
+    res.json({
+      status: 'success',
+      tour,
+    });
+  }
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body);
+  const newTour = { id: tours[tours.length - 1].id + 1, ...req.body };
+  tours.push(newTour);
+  fs.writeFile(PATH, JSON.stringify(tours), (err) => {
+    res.status(201).json({
+      status: 'success',
+      data: newTour,
+    });
   });
 });
 
