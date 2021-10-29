35575f04ae8a79860ef737c279358293864a3c25
Pranjal Gupta <pranjalg131@gmail.com>
2021-10-29 19:29:30 +0530

    Added esilnt and prettier configuration

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const dotenv = require('dotenv');
+
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
-
-console.log(process.env);
 
 app.listen(port, () => {
   console.log(`App is running on port ${port}...`);
