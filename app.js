1ba6a90c84acfc98d65b65e5eb09797888086251
Pranjal Gupta <pranjalg131@gmail.com>
2021-11-15 18:36:43 +0530

    Added handler for invalid routes and error middleware

--- app.js
+++ app.js
@@ -14,4 +14,25 @@ if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+// This section will be reached only when its not handled by the above ones
+app.all('*', (req, res, next) => {
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.status = err.status || 'error';
+  err.statusCode = err.statusCode || 500;
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+
+  next();
+});
+
 module.exports = app;
