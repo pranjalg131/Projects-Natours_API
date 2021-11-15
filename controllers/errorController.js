c48e12c3a1b47367b860abf96a6589882689a754
Pranjal Gupta <pranjalg131@gmail.com>
2021-11-15 22:33:35 +0530

    Added dev and production env specific errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,11 +1,40 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    error: err,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    // Trusted operaitonal error , can be sent to the end user.
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    // 1) Log the error
+    console.error(err);
+
+    // 2) Send a generic message to the end user.
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.status = err.status || 'error';
   err.statusCode = err.statusCode || 500;
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 
   next();
 };
--- utils/appError.js
+++ utils/appError.js
@@ -3,6 +3,7 @@ class appError extends Error {
     super(message);
     this.statusCode = statusCode;
     this.status = `${this.statusCode}`.startsWith('4') ? 'error' : 'fail';
+    this.isOperational = true;
 
     Error.captureStackTrace(this, this.constructor);
   }
