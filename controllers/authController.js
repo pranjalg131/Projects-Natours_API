6e80106e814cc598f9b5cc3a5ae57c598bc899c7
Pranjal Gupta <pranjalg131@gmail.com>
2021-11-16 20:20:39 +0530

    Fixed potential security bug and started implementing jwt based auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,26 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  // This is wrong as anyone could decide the roles for themselves
+  // const newUser = await User.create(req.body);
+  // Hence initially sign up the users with no privliges and then assign them as needed.
+
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+  });
+
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       newUser,
     },
