00149b13a161d6a2ddbcb0b5e3c49f6cf38aa750
Pranjal Gupta <pranjalg131@gmail.com>
2021-11-16 14:34:15 +0530

    Added global handler for unhandledRejections

--- server.js
+++ server.js
@@ -13,6 +13,15 @@ mongoose.connect(DB).then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App is running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('Unhandled Rejection, Shutting down...');
+  // server.close() quits gracefully rather than hard quitting like process.exit();
+  server.close(() => {
+    process.exit(1); // 0 -> no errors , 1 -> Unhandled Rejections.
+  });
 });
